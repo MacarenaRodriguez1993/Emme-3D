1b935ff9da1239d5233c9f1e0a40df65832d223d
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-12-06 19:28:32 -0300

    creacion de componente navBar

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import NavBar from "../components/NavBar/NavBar"
+import "./home.css"
+function Home() {
+    return (
+        <div className="home">
+            <NavBar />
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
