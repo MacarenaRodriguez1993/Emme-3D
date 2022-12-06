7d59da6549cdc13f6060e5c1df5bfb86644a7904
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-06 11:48:02 +0100

    #18
    - Añadir archivo para las rutas
    - Añadir carpeta para las rutas

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const server = express()
 const mongoose = require("mongoose")
+const routes = require("./routes/index.js")
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, PORT } = process.env
 const DEFAULT_PORT = 3001
@@ -18,6 +19,7 @@ mongoose
     })
 
 server.use(express.json())
+server.use("/", routes)
 
 // Si no tenemos un puerto especificado por variable global elegirá el puerto 3001 por defecto
 server.listen(PORT || DEFAULT_PORT, () => {
---
+++ api/routes/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router()
+// const productsRoute = require("./products") <--- Ejemplo de como debería ir
+
+// Routes
+// router.use("/productos", productsRoute) <--- Ejemplo de como debería ir
+
+module.exports = router
