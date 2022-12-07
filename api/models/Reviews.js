743c01f11d17148cac11a2c8c0bbbda3b0dea260
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-07 01:12:41 +0100

    Create Reviews.js
    Esquema de la tabla de valoraciones para la BBDD

---
+++ api/models/Reviews.js
@@ -0,0 +1,39 @@
+const { UUID } = require("bson")
+const mongoose = require("mongoose")
+
+/*
+ * Una Reseña es cualquier valoración hecha por un usuario.
+ * Contiene un producto valorado y un usuario que la escribe.
+ */
+
+const ReviewSchema = new mongoose.Schema(
+    {
+        // Id de la categoria
+        id: {
+            type: UUID,
+            required: true,
+            unique: true,
+        },
+
+        // Id del producto que se valora
+        product_id: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+        },
+
+        // Usuario que crea la valoración
+        user_id: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+        },
+
+        // Texto de la valoración
+        review: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("Review", ReviewSchema)
