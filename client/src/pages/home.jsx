7923c0502f38adffce096f28bf5c34f5ecfe61c4
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-12-06 23:54:41 -0300

    se creo component footer

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import NavBar from "../components/NavBar/NavBar"
+import Footer from "../components/Footer/Footer"
+
 import "./home.css"
 function Home() {
     return (
         <div className="home">
             <NavBar />
             <h1>Home</h1>
+            <Footer />
         </div>
     )
 }
