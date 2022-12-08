086fdd3efd0bfcca0a5ad452f1f707bb3edd88b5
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-12-08 14:06:09 -0300

    cambio en link navbar para redireccionar a landing

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div className="navBar">
             <ul>
-                <Link to="/home">
+                <Link to="/">
                     <h2>Emme 3D</h2>
                 </Link>
                 <div className="items">
