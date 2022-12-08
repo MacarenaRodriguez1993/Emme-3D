bcab15c97478010abb0ed667dd8ac95c6f95187b
oversightdolores <oversightdolores@gmail.com>
2022-12-08 14:01:19 -0300

    conflictos resueltos

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const DEFAULT_PORT = 3001
 
 mongoose
     .connect(
-        `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/?retryWrites=true&w=majority&ssl=true`
+        `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/?retryWrites=true&w=majority`
     )
     .then(() => {
         console.log("DB connection Successfull")
