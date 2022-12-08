ba6a0f71cb91eed19534fe4d563ed661470c165d
121218952415 <adrian.alexis.vega@gmail.com>
2022-12-08 12:42:58 -0600

    ruta id

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose")
 
 const listProducts = async () => {
-    const model = await Product.find(Product.ProductSchema)
+    const model = await Product.find({},(err,total)=>{
+         res.send({total})
+    })
+    
+}
+const ProducByQuery = async (name)=>{
+const byqu  =  prodduct.find({
+    name : name 
+},()=>{
+  
+})
 }
 const producdetails = (id) => {}
 
 module.exports = {
     producdetails,
     listProducts,
+    ProducByQuery
 }
--- api/routes/ProductsDetails.js
+++ api/routes/ProductsDetails.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 const producdetails = require("../controllers/index")
-
+const ProducByQuery = require("../controllers/index")
 
 router.get("/", async (req, res) => {
+    const {name} = req.query
     try {
         if (name) {
+        res.status(200).json(ProducByQuery(name))
         }
     } catch (err) {
         res.status(404).send(err.message)
