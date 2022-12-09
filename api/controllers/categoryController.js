f7b268168ccefc5d96dc73407680f9a303de21a5
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-09 14:22:15 +0100

    Update categoryController.js
    Añadido comentario

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -3,6 +3,7 @@ const Category = require("../models/Category")
 async function getCategories() {
     try {
         const categories = await Category.find()
+        // Si no hay categorías mandamos error
         if (categories.length < 1)
             throw new Error(
                 "Vaya, parece que no hay categorías en la base de datos."
