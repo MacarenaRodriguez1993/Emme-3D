13b5cdf7e661005e53fad10b4e7ca3c38949bfe2
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-09 18:47:53 +0100

    Update categoryController.js

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -21,7 +21,7 @@ async function createCategory(category) {
     } catch (error) {
         // E11000 es el código de error cuando el elemento ya existe (duplicado)
         if (error.message.includes("E11000"))
-            error.message = `Ya existe una categoría con nombre "${category.name}"`
+            error.message = `Ya existe una categoría con nombre "${category.name}".`
         throw error
     }
 }
