56ff7a2754c88dbd4cd95eda36595e08397694f0
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-09 13:36:56 +0100

    Update middlewareProducts.js
    Llamada del middleware

--- api/middleware/middlewareProducts.js
+++ api/middleware/middlewareProducts.js
@@ -4,6 +4,13 @@ const { productById } = require("../controllers/productController")
 function getAllProducts() {}
 
 async function getProductById(id) {
+    try {
+        const product = await productById(id)
+        // TO-DO: Faltará añadirle las categorías y reseñas
+        return product
+    } catch (error) {
+        throw error
+    }
 }
 
 function getProductByQuery() {}
