45f5bcf322b2e414062db472c6d49ff26c9efbae
oscar bejarano <osc4r.yt@gmail.com>
2022-12-11 21:43:22 -0500

    cssSearchBat

--- client/src/pages/Productos/products.jsx
+++ client/src/pages/Productos/products.jsx
@@ -27,8 +27,10 @@ const Products = () => {
         <div className="productos">
             <NavBar />
             {/* AQUI TIENEN QUE IR LOS FILTROS Y ORDENAMIENTOS */}
-            <SearchFilters />
-            <SearchByName />
+            <div className="barContainer">
+                <SearchFilters />
+                <SearchByName />
+            </div>
             <div className="cardProduct">
                 {error}
                 {productos?.map((a) => {
