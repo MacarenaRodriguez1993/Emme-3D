f9a647c7154aca0fcdeb1f2c16d51331a3695aa9
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-14 00:19:02 +0100

    Update CreateProduct.jsx
    Fix

--- client/src/components/CreateProduct/CreateProduct.jsx
+++ client/src/components/CreateProduct/CreateProduct.jsx
@@ -36,7 +36,7 @@ const CreateProduct = () => {
         setError(validations(producto))
         setProducto({
             ...producto,
-            categories_ids: [{ id: e.target.value, name: e.target.name }],
+            categories_ids: [e.target.value],
         })
     }
     const handleSubmit = (e) => {
