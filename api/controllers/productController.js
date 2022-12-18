d7935b54d6c07f932883b9cf3b95427957abf9e8
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-18 16:57:36 +0100

    Update productController.js
    Ahora los productos tienen un .category con la string de la categoría

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -21,7 +21,7 @@ const listProducts = async () => {
                 price: { $first: "$price" },
                 stock: { $first: "$stock" },
                 description: { $first: "$description" },
-                category: { $first: "$categories_ids" },
+                category: { $first: "$categories_ids.name" },
                 img: { $first: "$img" },
                 deleted: { $first: "$deleted" },
                 reviews_ids: { $first: "$reviews_ids" },
