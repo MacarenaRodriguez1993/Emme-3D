ab7cf4f1f33b66846a58e55364b4519a6644912e
oversightdolores <oversightdolores@gmail.com>
2022-12-28 16:00:55 -0300

    correcciones loging y loginGoogle

--- client/src/pages/perfil/Perfil.jsx
+++ client/src/pages/perfil/Perfil.jsx
@@ -1,13 +1,36 @@
 import React from "react"
-import { useSelector } from "react-redux"
-
+import { useDispatch, useSelector } from "react-redux"
+import { getAuth, signOut } from "firebase/auth"
+import { app } from "../../components/firebase/firebase"
+import { useNavigate } from "react-router-dom"
+import { getUsers } from "../../redux/actions/actions"
 export default function Perfil() {
     const user = useSelector((state) => state.users)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const auth = getAuth(app)
+    const logout = () => {
+        try {
+            signOut(auth)
+                .then(() => {
+                    // Sign-out successful.
+                    dispatch(getUsers(null))
+                    navigate("/login")
+                    console.log("sesion cerrada")
+                })
+                .catch((error) => {
+                    // An error happened.
+                })
+        } catch (err) {
+            console.log(err)
+        }
+    }
     console.log(user)
     return (
         <div>
             Perfil
-            <h2>{user.displayName} </h2>
+            <h2>{user?.displayName} </h2>
+            <button onClick={() => logout()}>cerrar sesion </button>
         </div>
     )
 }
