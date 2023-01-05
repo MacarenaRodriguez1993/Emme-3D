deed4a50c95233724fb4270b2bb4da3c24fb1a49
Cristian Lg <cristianflg1@hotmail.com>
2023-01-05 16:45:52 -0300

    agregue boton para editar imagen en el perfil de usuario

--- client/src/components/UserPanel/UserPanel.jsx
+++ client/src/components/UserPanel/UserPanel.jsx
@@ -1,14 +1,19 @@
 import "./UserPanel.css"
+import { AiTwotoneEdit } from "react-icons/ai"
 
 const UserPanel = ({ user, logout }) => {
     return (
         <div className="user-panel-container">
             <div className="user-info">
-                <img
-                    src={user?.photoURL}
-                    alt={user?.photoURL}
-                    className="user-img"
-                />
+                <div className="user-img-cont">
+                    <img
+                        src={user?.photoURL}
+                        alt={user?.photoURL}
+                        className="user-img"
+                    />
+                    <AiTwotoneEdit className="img-edit-icon" />
+                </div>
+
                 <div className="user-name-email">
                     <p className="welcome-user">
                         ¡Bienvenido {user?.displayName}!
@@ -21,11 +26,13 @@ const UserPanel = ({ user, logout }) => {
             </div>
             <p className="user-data">Tus datos</p>
             <p className="user-data-fields">Nombre: </p>
+            <p className="user-data-fields">Apellido: </p>
+            <p className="user-data-fields">Telefono: </p>
             <p className="user-data-fields">Dirección: </p>
             <p className="user-data-fields">Ciudad: </p>
             <p className="user-data-fields">Provincia: </p>
             <p className="user-data-fields">Código postal: </p>
-            <button className="user-data-fields user-logout">
+            <button className="user-data-fields user-logout user-edit">
                 Editar mi información
             </button>
             <p className="user-data">Tus pedidos</p>
