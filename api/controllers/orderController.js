34e5325a3959d2a16cb05116026ca54835b85afb
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-01-05 13:41:37 +0100

    Crear pedidos OK

---
+++ api/controllers/orderController.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const Order = require("../models/Order.js")
+const ObjectId = mongoose.Types.ObjectId
+
+async function createOrder(order) {
+    try {
+        console.log(order)
+        order.user_id = ObjectId(order.user_id)
+        const createdOrder = await Order.create(order)
+        return createdOrder
+    } catch (error) {
+        throw error
+    }
+}
+
+async function getOrders() {
+}
+
+async function getUserOrders(id) {}
+
+async function updateOrder(id, order) {}
+
+module.exports = {
+    createOrder,
+    getOrders,
+    getUserOrders,
+    updateOrder,
+}
