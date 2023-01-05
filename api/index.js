0b72026913a2dec5b55b5d79bddd19887c407f53
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-01-05 12:33:40 -0300

    cors

--- api/index.js
+++ api/index.js
@@ -23,6 +23,19 @@ mongoose
     })
 
 server.use(cors())
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    )
+    next()
+})
 server.use(express.json())
 server.use(morgan("dev")) //agrege morgan
 server.use("/", routes)
