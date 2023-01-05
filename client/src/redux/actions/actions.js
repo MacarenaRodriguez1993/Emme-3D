e1759d5ddaa49265d53cb1827e972865c00631b7
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-01-05 12:28:52 -0300

    actualizacion

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -99,7 +99,7 @@ export const filterByLikes = (value) => {
 /*--------- ACTIONS POST -------------*/
 //Aqui va la url base del back
 //let url_api = "http://localhost:3001"
-let url_api = "https://emme-3d-production-c491.up.railway.app/"
+let url_api = "https://emme-3d-production-c491.up.railway.app"
 
 //Action para postear productos
 export const postProduct = (product) => {
