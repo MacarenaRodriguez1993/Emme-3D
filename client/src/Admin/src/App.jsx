2053331b1ca75b1e1461a9fb6796f8c515ded035
Cristian Lg <cristianflg1@hotmail.com>
2023-01-07 22:27:01 -0300

    cambie el favicon y el titulo del sitio

--- client/src/Admin/src/App.jsx
+++ client/src/Admin/src/App.jsx
@@ -1,16 +1,16 @@
-import Sidebar from "./components/Sidebar/Sidebar";
-import Topbar from "./components/Topbar/Topbar";
-import "./App.css";
-import Home from "./pages/home/Home";
+import Sidebar from "./components/Sidebar/Sidebar"
+import Topbar from "./components/Topbar/Topbar"
+import "./App.css"
+import Home from "./pages/home/Home"
 
 export default function App() {
-  return (
-    <div>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Home />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Topbar />
+            <div className="container">
+                <Sidebar />
+                <Home />
+            </div>
+        </div>
+    )
 }
